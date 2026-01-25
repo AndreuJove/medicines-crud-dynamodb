67a22aa331e10a4afb7a4d14d2129f4edbd7ebc1
AndreuJove <andreujove@gmail.com>
2026-01-25 12:03:39 +0100

    Rename project folder

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from chalice import Chalice
 
-from medicine_crud.application.controller.routes import medicine_crud_routes
+from medicines.application.controller.routes import medicine_crud_routes
 
 app = Chalice(app_name="medicine_crud")
 app.register_blueprint(medicine_crud_routes)
